40667fb24c0b42a8a391632cb75e1fa732ddeed9
Héctor Hurtado <hector.hurtado@bbva.com>
2020-06-24 14:44:34 +0200

    Recover Go releases info and generate file

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+const fs = require('fs');
+const request = require('request');
 const core = require('@actions/core');
 
-async function run() {
+function run() {
   try {
     const goVersionFile = core.getInput('go-version');
-    let currentGoVersion = ''
     let newGoVersion = ''
 
-    console.log(`Looking for version in path ${goVersionFile}`)
+//    if (!fs.existsSync(goVersionFile)) {
+//      console.log(`File do not exists: ${goVersionFile}`)
+//      return process.exit(1);
+//    }
 
-    if (currentGoVersion !== newGoVersion) {
+    request('https://golang.org/dl/?mode=json', { json: true }, (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      for (let release of body) {
+        if (release.stable) {
+          newGoVersion = release.version.substr(2);
+          break;
+        }
+      }
+
+      fs.writeFile(goVersionFile, newGoVersion, function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+
+      core.setOutput('go-version', `${newGoVersion}`);
       core.setOutput('pr-title', `Update to latest Go release ${newGoVersion}`);
       core.setOutput('branch-name', `refresh-go/patch-${newGoVersion}`);
-    }
+    });
   }
   catch (error) {
     core.setFailed(error.message);
